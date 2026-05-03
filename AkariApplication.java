6bb3f23b983008b2c04f21ae7427fd7180187bd4
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-03 18:16:12 -0300

    Arquivo main

---
+++ AkariApplication.java
@@ -0,0 +1,13 @@
+package com.web.akari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkariApplication.class, args);
+	}
+
+}
