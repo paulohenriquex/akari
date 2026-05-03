0021ddfee3bdd2cc5878cd83c191eb3e8c686d5f
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-03 18:16:58 -0300

    Criação da classe Ingrediente

---
+++ model/Ingrediente.java
@@ -0,0 +1,35 @@
+package com.web.akari.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Ingrediente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "ficha_tecnica_id")
+    private FichaTecnica fichaTecnica;
+
+    private Double percapita;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
