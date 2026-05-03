8a92f377a44defb1bd2f9eacd57b562e1f48beaf
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-03 18:17:37 -0300

    Criação da classe Servico

---
+++ model/Servico.java
@@ -0,0 +1,27 @@
+package com.web.akari.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Servico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
