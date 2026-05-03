c48b0bd8cbaaf59a5b88a2c24102d71dddbd77ba
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-03 18:16:23 -0300

    Criação da classe Categoria

---
+++ model/Categoria.java
@@ -0,0 +1,33 @@
+package com.web.akari.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Categoria {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   private String nome;
+
+   @OneToMany(mappedBy = "categoria")
+   private List<Produto> produto;
+
+   @ManyToOne
+   @JoinColumn(name = "user_id")
+   private User user;
+
+}
