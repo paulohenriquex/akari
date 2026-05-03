e26669defbb52631b1f3a76d4ed8df04e3b3e398
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-03 18:17:13 -0300

    Criação da classe Planejamento

---
+++ model/Planejamento.java
@@ -0,0 +1,38 @@
+package com.web.akari.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Planejamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "servico_id")
+    private Servico servico;
+
+    @ManyToOne
+    @JoinColumn(name = "fica_tecnica_id")
+    private FichaTecnica fichaTecnica;
+
+    private int quantitativo;
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
