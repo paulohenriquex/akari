2c65a47d30663ef68671a3dd557a5250a26a2481
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-04 10:21:30 -0300

    Criação do UserRepository

---
+++ repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.web.akari.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.web.akari.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
