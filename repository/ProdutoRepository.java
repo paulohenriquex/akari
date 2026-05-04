76431b24a6bf752caa2b08eede7c44a14ab9b45d
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-04 10:21:22 -0300

    Criação do ProdutoRepository

---
+++ repository/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.web.akari.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.web.akari.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> findByUserId(Long userId);
+
+}
