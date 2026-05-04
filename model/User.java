fdad1bd0ba2ac184884c3ee1d0a674d08dcb78d5
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-04 10:20:35 -0300

    Mudando o nome da tabela user.

--- model/User.java
+++ model/User.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "tb_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
