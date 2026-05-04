f6f65c0b0a4144e9801fe7158b7fed4f973c0f40
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-04 10:20:21 -0300

    Criação do ProdutoResponseDTO.

---
+++ dto/ProdutoResponseDTO.java
@@ -0,0 +1,12 @@
+package com.web.akari.dto;
+
+import java.math.BigDecimal;
+
+public record ProdutoResponseDTO(
+        Long id,
+        String nome,
+        BigDecimal preco,
+        String medida,
+        String nomeMarca,
+        String nomeCategoriaId) {
+}
