b906797a6332018c98c5604bd2092ece3505a41a
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-05 10:04:53 -0300

    Criação do Repository Marca.

--- repository/MarcaRepository.java
+++ repository/MarcaRepository.java
@@ -1,9 +1,11 @@
 package com.web.akari.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.web.akari.model.Marca;
 
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
-
+    List<Marca> findByUserId(Long userId);
 }
