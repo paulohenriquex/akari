b5b5afa958f6c16b4b0728512a69ff3256bf3692
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-05 10:04:41 -0300

    Criação do Repository Categoria.

--- repository/CategoriaRepository.java
+++ repository/CategoriaRepository.java
@@ -1,9 +1,11 @@
 package com.web.akari.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.web.akari.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
+    List<Categoria> findByUserId(Long userId);
 }
