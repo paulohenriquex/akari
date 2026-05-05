1af6914babba422324f1d1745a924635869743e9
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-05 10:03:48 -0300

    Criação do Controller Marca.

---
+++ controller/MarcaController.java
@@ -0,0 +1,36 @@
+package com.web.akari.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.akari.dto.MarcaRequestDTO;
+import com.web.akari.dto.MarcaResponseDTO;
+import com.web.akari.service.MarcaService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/marcas")
+@RequiredArgsConstructor
+public class MarcaController {
+
+    private final MarcaService marcaService;
+
+    @PostMapping
+    public ResponseEntity<MarcaResponseDTO> cadastrar(@RequestBody MarcaRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(marcaService.criar(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MarcaResponseDTO>> listarPorUsuario(@PathVariable Long userId) {
+        return ResponseEntity.ok(marcaService.listarPorUsuario(userId));
+    }
+}
